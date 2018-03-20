4ffbe193c63853576a0fa8eada6e3381a790319c
kristina nette <kristina.nette@gmail.com>
2018-03-20 10:21:28 -0700

    updated project model with new schema and updated edit and new forms

--- models/Project.js
+++ models/Project.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
 const projectSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    url: String,
+    githubUrl: String,
     description: String,
     imageUrl: String,
+    projectUrl: String,
+    currentProject: {type: Boolean, default: true},
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'} 
 })
 
